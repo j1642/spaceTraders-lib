b75a167cb50032513805b75c1eb52c2620274a13
j1642 <60148902+j1642@users.noreply.github.com>
2023-05-12 15:01:16 -0400

    ENH: Add automated mining and selling using json.Unmarshal

--- main.go
+++ main.go
@@ -10,9 +10,12 @@ import (
 	"os"
 	"strings"
     "strconv"
-    //"time"
+    "time"
+
+    "spacetraders/objects"
 )
 
+var miningShip = readMiningShipName()
 var auth string = generateAuth()
 var client *http.Client = &http.Client{}
 
@@ -20,13 +23,32 @@ func main() {
     //listWaypointsInSystem()
     //travelToAsteroidField()
     //listMyShips()
-    //orbitLocation("shipName")
-    extractOre("shipName")
+    //orbitLocation(miningShip)
     //localMarket()
     //viewContract() // Aluminum ore
-    //dockShip("shipName")
-    //listCargo("shipName")
-    //sellCargo("shipName", "SILICON_CRYSTALS", 2)
+    for i := 0; i < 10; i++ {
+        extractOre(miningShip, 5)
+        dockShip(miningShip)
+        sellCargoBesidesAl(miningShip)
+    }
+    //fmt.Println(describeShip(miningShip).Ship.Fuel)
+    //sellCargo(miningShip, "ICE_WATER", 7)
+}
+
+func sellCargoBesidesAl(ship string) {
+    for {
+        cargo := describeShip(ship).Ship.Cargo.Inventory
+        if len(cargo) == 1 {
+            fmt.Println(cargo)
+            break
+        }
+        for _, item := range cargo {
+            if item.Symbol[0:2] != "AL" {
+                fmt.Println("Selling", item.Symbol)
+                sellCargo(ship, item.Symbol, item.Units)
+            }
+        }
+    }
 }
 
 func sellCargo(ship, item string, amount int) {
@@ -40,11 +62,19 @@ func sellCargo(ship, item string, amount int) {
     sendRequest(req)
 }
 
-func listCargo(ship string) {
+func describeShip(ship string) objects.DataShip{
+    // Lists cargo.
     urlPieces := []string{"https://api.spacetraders.io/v2/my/ships/", ship}
     url := strings.Join(urlPieces, "")
     req := makeRequest("GET", url, nil)
-    sendRequest(req)
+    out := sendRequest(req)
+    //out.WriteTo(os.Stdout)
+    var data objects.DataShip
+    err := json.Unmarshal(out.Bytes(), &data)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return data
 }
 
 func viewContract() {
@@ -60,12 +90,25 @@ func localMarket() {
     sendRequest(req)
 }
 
-func extractOre(ship string) {
+func extractOre(ship string, repeat int) {
     // Similar to dockShip(), refuelShip()
+    orbitLocation(ship)
     urlPieces := []string{"https://api.spacetraders.io/v2/my/ships/", ship, "/extract"}
     url := strings.Join(urlPieces, "")
     req := makeRequest("POST", url, nil)
-    sendRequest(req)
+    for i := 0; i < repeat; i++ {
+        cargo := describeShip(ship).Ship.Cargo
+        if cargo.Units == cargo.Capacity {
+            fmt.Println("cargo full")
+            break
+        }
+        sendRequest(req)
+        fmt.Println("Extraction #", i, " cargo", cargo.Units, "/", cargo.Capacity)
+        if i == (repeat-1) {
+            break
+        }
+        time.Sleep(70 * time.Second)
+    }
 }
 
 func orbitLocation(ship string) {
@@ -86,19 +129,22 @@ func refuelShip(ship string) {
 
 func dockShip(ship string) {
     // Similar to refuelShip()
+    refuelShip(ship)
     urlPieces := []string{"https://api.spacetraders.io/v2/my/ships/", ship, "/dock"}
     url := strings.Join(urlPieces, "")
     req := makeRequest("POST", url, nil)
-    sendRequest(req)
+    sendRequest(req).WriteTo(os.Stdout)
 }
 
-func travelToAsteroidField() {
+func travelToAsteroidField(ship string) {
     jsonContent := []byte(
 `{
 "waypointSymbol": "X1-DF55-17335A"
 }`)
     
-    req := makeRequest("POST", "https://api.spacetraders.io/v2/my/ships/shipName/navigate", jsonContent)
+    urlPieces := []string{"https://api.spacetraders.io/v2/my/ships/", ship, "/navigate"}
+    url := strings.Join(urlPieces, "")
+    req := makeRequest("POST", url, jsonContent)
     req.Header.Set("Content-Type", "application/json")
     sendRequest(req)
 }
@@ -145,7 +191,7 @@ func makeRequest(httpMethod, url string, msg []byte) *http.Request {
     return request
 }
 
-func sendRequest(request *http.Request) {
+func sendRequest(request *http.Request) *bytes.Buffer {
 	resp, err := client.Do(request)
 	if err != nil {
 		log.Fatalf("%v", err)
@@ -154,8 +200,9 @@ func sendRequest(request *http.Request) {
 
 	body, err := io.ReadAll(resp.Body)
 	var out bytes.Buffer
-	json.Indent(&out, body, "=", "    ")
-	out.WriteTo(os.Stdout)
+	json.Indent(&out, body, "", "    ")
+	//out.WriteTo(os.Stdout)
+    return &out
 }
 
 func generateAuth() string {
@@ -166,4 +213,14 @@ func generateAuth() string {
 	auth := fmt.Sprintf("Bearer %s", key)
 	auth = strings.ReplaceAll(auth, "\n", "")
 	return auth
+}
+
+func readMiningShipName() string {
+	name, err := os.ReadFile("miningDrone.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+    strName := string(name)
+	strName = strings.ReplaceAll(strName, "\n", "")
+	return strName
 }
