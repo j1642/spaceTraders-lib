440e6f3ba7ee86b33b1a6adfc9ec128930050118
j1642 <60148902+j1642@users.noreply.github.com>
2024-04-30 12:22:37 -0400

    ENH: Style nav, add agent name HTML template, finish basic agent name HTMX

--- server.go
+++ server.go
@@ -7,18 +7,20 @@ import (
 	"io"
 	"log"
 	"net/http"
-    "strings"
-	//"github.com/j1642/spaceTraders-lib/objects"
-	//"github.com/j1642/spaceTraders-lib/requests"
+	"strings"
 )
 
 var agentName string
 
 func main() {
+	runServer()
+}
+
+func runServer() {
 	temIndex := template.Must(template.New("dashboard.html").ParseFiles("dashboard.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Println("/")
-		err := temIndex.Execute(w, ' ')
+		fmt.Println("/")
+		err := temIndex.Execute(w, agentName)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -29,6 +31,9 @@ func main() {
 	})
 	http.HandleFunc("/style.css", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "style.css")
+	})
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "about.html")
 	})
 
 	http.HandleFunc("/server-check", func(w http.ResponseWriter, r *http.Request) {
@@ -49,40 +54,36 @@ func main() {
 		resp.Body.Close()
 	})
 
+	temRegister := template.Must(template.New("register.html").ParseFiles("register.html"))
 	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
-        if r.Method == "PUT" {
-            body, err := io.ReadAll(r.Body)
-            if err != nil {
-                log.Fatal(err)
-            }
-            requestData := strings.Split(string(body), "&")
-            for i := range requestData {
-                attrValue := strings.Split(requestData[i], "=")
-                if attrValue[0] == "agent" {
-                    agentName = attrValue[1]
-                    fmt.Println(attrValue)
-                }
-            }
-        // TODO: make template for PUT to /register (pass agent name from Request)
-            return
-        }
-		http.ServeFile(w, r, "register.html")
-        fmt.Println("register")
-        /*_, err := w.Write([]byte(`<div hx-target="this" hx-swap="outerHTML">
-            <div><label>First Name</label>: Joe</div>
-            <div><label>Last Name</label>: Blow</div>
-            <div><label>Email</label>: joe@blow.com</div>
-            <button hx-get="/register-new" class="btn btn-primary">
-            Click To Edit
-            </button>
-            </div>`,
-        ))*/
-
+		if r.Method == "PUT" {
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			requestData := strings.Split(string(body), "&")
+			for i := range requestData {
+				attrValue := strings.Split(requestData[i], "=")
+				if attrValue[0] == "agent" {
+					agentName = attrValue[1]
+					fmt.Println(attrValue)
+				}
+			}
+			err = temRegister.Execute(w, agentName)
+			if err != nil {
+				log.Fatal(err)
+			}
+			return
+		}
+		err := temRegister.Execute(w, agentName)
+		if err != nil {
+			log.Fatal(err)
+		}
 		/*_, err := w.Write([]byte(`<!DOCTYPE html><div hx-target="this" hx-swap="outerHTML">
-            <div><label>Name</label>: Placeholder</div>
-            <button hx-get="/register-new">Edit agent</button>
-        </div>`,
-		))*/
+		            <div><label>Name</label>: Placeholder</div>
+		            <button hx-get="/register-new">Edit agent</button>
+		        </div>`,
+				))*/
 		/*if err != nil {
 			log.Fatal(err)
 		}*/
@@ -90,19 +91,19 @@ func main() {
 
 	http.HandleFunc("/register-new", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "register-new.html")
-        fmt.Println("register-new")
+		fmt.Println("register-new")
 		/*_, err := w.Write([]byte(`<!DOCTYPE html><form hx-put="/register" hx-target="this" hx-swap="outerHTML">
-        <div>
-            <label>Agent</label>
-            <input type="text" name="agent" value="New agent name">
-        </div>
-        <button>Submit</button>
-        <button hx-get="/register">Cancel</button>
-        </form>`,
-		))
-		if err != nil {
-			log.Fatal(err)
-		}*/
+		        <div>
+		            <label>Agent</label>
+		            <input type="text" name="agent" value="New agent name">
+		        </div>
+		        <button>Submit</button>
+		        <button hx-get="/register">Cancel</button>
+		        </form>`,
+				))
+				if err != nil {
+					log.Fatal(err)
+				}*/
 	})
 
 	fmt.Println("Server listening on 8080")
